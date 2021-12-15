244da9e33b40c1d2e205eac550223dc517611cea
Monicacyu <96177286+Monicacyu@users.noreply.github.com>
2021-12-15 10:29:36 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
 basic.forever(function () {
     basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
         . # . # .
-        # # # # #
-        # # # # #
         . # # # .
         . . # . .
+        . . . . .
         `)
-    music.playMelody("A B C5 B A A B C ", 120)
     basic.showLeds(`
         . # . # .
         # # # # #
