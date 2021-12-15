fcdcaf4e79bee6acd45caa207ab2d3b5cfa7c61a
Monicacyu <96177286+Monicacyu@users.noreply.github.com>
2021-12-15 10:17:31 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    music.playMelody("A B C5 B A A B C ", 120)
+})
